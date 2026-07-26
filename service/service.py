70535bc176bbe84f709c443b2438145ffac4216d
yasamanlotfian <yasamanlotfian388@gmail.com>
2026-07-26 16:34:19 +0330

    Update service CRUD logic

--- service/service.py
+++ service/service.py
@@ -4,14 +4,13 @@ from sqlalchemy.orm import Session
 from database import get_db
 
 from models.appointment import Appointment
-from models.room import Room
+from models.employee import Employee
 from models.service import Service
 
 from schemas.appointments import (
     AppointmentCreate,
     AppointmentResponse
 )
-
 
 router = APIRouter(
     prefix="/appointments",
@@ -29,20 +28,32 @@ def create_appointment(
         Service.id == appointment.service_id
     ).first()
 
-
     if not service:
         raise HTTPException(
             status_code=404,
             detail="Service not found"
         )
 
-
-    room = db.query(Room).filter(
-        Room.id == appointment.room_id
+    employee = db.query(Employee).filter(
+        Employee.id == appointment.employee_id
     ).first()
 
-if not room:
-    raise HTTPException(
-        status_code=404,
-        detail="Room not found"
+    if not employee:
+        raise HTTPException(
+            status_code=404,
+            detail="Employee not found"
+        )
+
+    new_appointment = Appointment(
+        user_id=appointment.user_id,
+        employee_id=appointment.employee_id,
+        service_id=appointment.service_id,
+        start_time=appointment.start_time,
+        end_time=appointment.end_time
     )
+
+    db.add(new_appointment)
+    db.commit()
+    db.refresh(new_appointment)
+
+    return new_appointment
