72c65c7ccae469e930998c10e0bdb13e90174e29
yasamanlotfian <yasamanlotfian388@gmail.com>
2026-07-27 10:32:26 +0330

    Add Service model

---
+++ models/service.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+
+from database import Base
+
+
+class Service(Base):
+    __tablename__ = "services"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    description = Column(String(255), nullable=True)
+    price = Column(Integer, nullable=False)
+    duration = Column(Integer, nullable=False)
